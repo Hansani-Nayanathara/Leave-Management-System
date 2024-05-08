c56bdd248b8f14510b4d8604f81928b23e6df727
Hansani Nayanathara <hansaninayanathara422@gmail.com>
2024-05-08 21:26:51 +0530

    file

---
+++ Leave Management System/Passtxt/Passtxt/passtxt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Passtxt
+{
+    public partial class passtxt: TextBox
+    {
+        public passtxt()
+        {
+            BackColor = Color.DimGray;
+            ForeColor = Color.White;
+            HasLimit = true;
+
+        }
+        [Category ("Behaviour")] public bool HasLimit { get; set; }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            if (HasLimit)
+            {
+                MaxLength = 10;
+                PasswordChar = '*';
+            }
+            else
+            {
+                e.Handled = false;
+            }
+           
+                
+        }
+
+    }
+}
